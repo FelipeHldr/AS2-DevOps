5fa3e78d4ee02139f05c20c15d7df579abd863e7
Felipe Alberto <86085023+FelipeHldr@users.noreply.github.com>
2023-04-22 23:21:18 -0300

    Jesus amado...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,8 +10,14 @@ def index():
 def login():
     username = request.form['username']
     password = request.form['password']
-    # Verifique as credenciais do usuário aqui
-    return f'Bem-vindo {username}'
+    if username == 'admin' and password == 'admin':
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
